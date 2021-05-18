ed78a34fe80ae41042ca85af6699d28ade128a70
Bluxy_ <48993224+RealBluxy@users.noreply.github.com>
2021-05-18 22:46:05 +0100

    Update Task.php

--- src/com/Bluxy/Task.php
+++ src/com/Bluxy/Task.php
@@ -1,5 +1,35 @@
 <?php
 
+/*
+* " Hey? What Are You Doing Here ? Unless You Know What You are Doing ,  You Should Probaply Close This File And Stay Safe ! "
+*
+*
+*     
+*╭━━╮╭╮
+*┃╭╮┃┃┃
+*┃╰╯╰┫┃╭╮╭┳╮╭┳╮╱╭╮
+*┃╭━╮┃┃┃┃┃┣╋╋┫┃╱┃┃
+*┃╰━╯┃╰┫╰╯┣╋╋┫╰━╯┃
+*╰━━━┻━┻━━┻╯╰┻━╮╭╯
+*╱╱╱╱╱╱╱╱╱╱╱╱╭━╯┃
+*╱╱╱╱╱╱╱╱╱╱╱╱╰━━╯
+*
+*                Copyright (C) <2021>  <Bluxy>
+*
+*    This program is free software: you can redistribute it and/or modify
+*    it under the terms of the GNU General Public License as published by
+*    the Free Software Foundation, either version 3 of the License, or
+*    any later version.
+*
+*    This program is distributed in the hope that it will be useful,
+*    but WITHOUT ANY WARRANTY; without even the implied warranty of
+*    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*    GNU General Public License for more details.
+*
+*    Note : If You Found Any Isuess Or Suggestions Please Contact Me On Discord : "Blux#4061"
+*
+*/
+
 namespace com\Bluxy;
 
 use pocketmine\scheduler\Task as MsgTask;
